0198469c7f9bc34f298690cb228b2e2e5cea7dc5
Olivier M. Mukadi <mbo2olivier@gmail.com>
2019-11-03 15:16:31 +0100

    change APPEND mutation to just add element in target instead of empty all content

--- src/mutations/append.ts
+++ src/mutations/append.ts
@@ -3,8 +3,12 @@ import { Uijx } from '../core';
 const APPEND = {
     name: "APPEND",
     change(data:string,target:Element, targetedAttrib:string|null,mutationParams:Array<string|null>=[],$:Uijx):void {
-        target.innerHTML += data;
+        var div = document.createElement('div');
+        div.innerHTML = data;
         $.disconnect(target);
+        while(div.firstChild) {
+            target.appendChild(div.firstChild);
+        }
         $.connect(target);
         $.dispatch('mutated', target, {mutation: 'APPEND', data: data, attrib: targetedAttrib});
     }
