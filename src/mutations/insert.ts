bb27d610014671a396806a55868c0600e4e8d609
Olivier M. Mukadi <mbo2olivier@gmail.com>
2019-11-06 12:50:28 +0100

    [bug fix] connect/disconnect triggers when applying INSERT mutation

--- src/mutations/insert.ts
+++ src/mutations/insert.ts
@@ -6,7 +6,9 @@ const INSERT = {
         if(typeof params[0] === 'string') {
             let position = params[0].replace(/^\s+|\s+$/g, '');
             if(position == 'beforebegin' || position == 'afterbegin' || position == 'beforeend' || position == 'afterend') {
+                $.disconnect(target);
                 target.insertAdjacentHTML(position, data);
+                $.connect(target);
                 $.dispatch('mutated', target, {mutation: 'INSERT', data: data, attrib: targetedAttrib});
             }else
                 throw new Error('position must be on of values: beforebegin, afterbegin, beforeend or afterend');
