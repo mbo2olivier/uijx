e34fa2a033e260ffceb23c0a6ae31f555435d7c8
Olivier M. Mukadi <mbo2olivier@gmail.com>
2020-03-13 13:49:37 +0100

    [fix bug] addClass dispatch right event on preformed

--- src/mutations/addClass.ts
+++ src/mutations/addClass.ts
@@ -5,7 +5,7 @@ const ADDCLASS = {
     change(data:any,target:Element, targetedAttrib:string|null,params:Array<string|null>=[],$:Uijx):void {
         if(typeof params[0] === 'string') {
             target.classList.add(params[0]);
-            $.dispatch('mutated', target, {mutation: 'RMCLASS', data: params[0], attrib: targetedAttrib});
+            $.dispatch('mutated', target, {mutation: 'ADDCLASS', data: params[0], attrib: targetedAttrib});
         }
         else
             throw new Error('classname to toggle is missing');
