19b22b2c9880873c962a0c51956ef5228a9d61e8
Olivier M. Mukadi <mbo2olivier@gmail.com>
2020-04-06 23:41:35 +0200

    set default trigger data for event: blur, change, focus, keydow, keyup

--- src/triggers/event.js
+++ src/triggers/event.js
@@ -3,12 +3,19 @@ export default {
     attachTo: 'self',
     event: null,
     handle (slot, event, info, $, callback) {
-        $.run(info.before, slot, undefined, (data) => {
+        let data = undefined;
+        
+        if(['blur', 'change', 'focus', 'keydown', 'keyup'].indexOf(event.type) >= 0) {
+            data = slot.$el.value;
+        }
+
+        $.run(info.before, slot, data, (d) => {
+            d = d || data;
             if(info.hasModifier('prevent')) {
                 event.preventDefault();
             }
 
-            $.run(info.task,slot, data, (data) => $.run(info.after, slot, data, callback));
+            $.run(info.task,slot, d, (d) => $.run(info.after, slot, d, callback));
         });
     }
 }
