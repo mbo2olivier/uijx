ebc1d199abdeb3f2b0d8f9913e22b5d7b1e40820
Olivier M. Mukadi <mbo2olivier@gmail.com>
2020-05-04 12:01:05 +0100

    add 'self' modifier on waiting trigger

--- src/triggers/waiting.js
+++ src/triggers/waiting.js
@@ -5,8 +5,19 @@ export default {
     waiting: false,
     handle (slot, event, info, $) {
         let data = event.detail.waiting;
+        let getIn = false;
 
-        if(info.hasModifier(event.detail.slot.id) || !info.containModifiers()) {
+        if(info.hasModifier('self')) {
+            getIn = slot.$el === event.detail.slot;
+        }
+        else if(info.hasModifier(event.detail.slot.id)) {
+            getIn = true;
+        }
+        else if(!info.containModifiers()){
+            getIn = true;
+        }
+
+        if(getIn) {
             
             return $.run(info.before, slot, data)
                     .then((d) => {
